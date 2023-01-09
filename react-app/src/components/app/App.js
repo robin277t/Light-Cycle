5bd4c2d6f2a739d383b69a06e5ad6c5dd152e1a2
Henry <boylanh3nry@gmail.com>
2023-01-09 11:47:28 +0000

    Start menu has been integrated with gamover screen

--- react-app/src/components/app/App.js
+++ react-app/src/components/app/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import "./index.scss";
-import "./StartMenu";
+import "../menus/StartMenu";
 // import GameLoop from "../grid/GameLoop.js";
 import GameOver from "../menus/GameOver.js";
+import StartMenu from "../menus/StartMenu";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <a
           className="App-link"
           href="https://www.youtube.com/watch?v=cFuRPMjYKhs&t=181s"
@@ -16,8 +17,9 @@ function App() {
         >
           Light Cycle is gonna be AWESOME!!! Check out a video of it here...
         </a>
-      </header>
-      <GameOver message="Ignore above message-- click 'PLAY AGAIN' to start a new game" />
+      </header> */}
+      <StartMenu />
+      {/* <GameOver message="Ignore above message-- click 'PLAY AGAIN' to start a new game" /> */}
     </div>
   );
 }
--- react-app/src/components/menus/StartMenu.js
+++ react-app/src/components/menus/StartMenu.js
@@ -1,12 +1,24 @@
+import React, { useState } from "react";
+import "../app/index.scss";
+import "../app/App.css";
+import GameLoop from "../grid/GameLoop.js";
+
 const StartMenu = () => {
-  return (
-    <div classname='menu-container'>
-      <h2 classname='menu-item'>
+  const [menuSelect, setMenuSelect] = useState('none');
+
+  const selectMenu = (gamemode) => {
+    setMenuSelect(gamemode);
+  };
+
+  if (menuSelect === 'none') {
+    return (
+      <div classname="menu-container">
+        <h2 classname="menu-item">
           <body>
-          <button type='button' classname='button'>
-            <a href='/Singleplayer'>Singleplayer</a>
-          </button>
-          <button>
+            <button type="button" classname="button" onClick={() => selectMenu('multiplayer')}>
+              Multiplayer
+            </button>
+            {/* <button>
             <a href='/Multiplayer'>Multiplayer</a>
           </button>
           <button>
@@ -14,12 +26,23 @@ const StartMenu = () => {
           </button>
           <button>
             <a href='/Leaderboard'>Leaderboard</a>
-          </button>
+          </button> */}
           </body>
-      </h2>
-    </div>
-  )
-}
+        </h2>
+      </div>
+    );
+  } else {
+    return (
+      <GameLoop
+        gridSide={50}
+        trailLength={100}
+        gameSpeed={30}
+        player1={"player1"}
+        player2={"player2"}
+      />
+    );
+  }
+};
 
 export default StartMenu;
 
