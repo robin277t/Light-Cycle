9aaaf5220f6a421f902ee0cd5268bc202624edb2
Henry <boylanh3nry@gmail.com>
2023-01-09 10:33:18 +0000

    Start menu completed to a MVP stage

--- react-app/src/components/app/App.js
+++ react-app/src/components/app/App.js
@@ -1,18 +1,22 @@
-import './App.css';
+import "./App.css";
+import "./index.scss";
+import "./StartMenu";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://www.youtube.com/watch?v=cFuRPMjYKhs&t=181s"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Light Cycle is gonna be AWESOME!!! Check out a video of it here...
-        </a>
-      </header>
+      <div className="menu-container">
+        <header className="App-header">
+          <a
+            className="App-link"
+            href="https://www.youtube.com/watch?v=cFuRPMjYKhs&t=181s"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Light Cycle is gonna be AWESOME!!! Check out a video of it here...
+          </a>
+        </header>
+      </div>
     </div>
   );
 }
