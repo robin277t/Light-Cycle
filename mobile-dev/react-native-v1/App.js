61fe89d819275f859f5f841059f02475ef8cea68
Henry <boylanh3nry@gmail.com>
2023-01-09 16:32:58 +0000

    Pop messages successfully implemented with timeout of 5 seconds after appearing

--- mobile-dev/react-native-v1/App.js
+++ mobile-dev/react-native-v1/App.js
@@ -1,45 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
+import ModalTemplate from "./modal-template";
 
 export default function App() {
-  const testFunction = () => {
-    return <Text>LeftMate</Text>;
-  };
-  return (
-    <>
-      <View style={styles.container}>
-        <Text>Welcome to the Light Cycle mobile app</Text>
-        <StatusBar style="auto" />
-      </View>
-      <View style={styles.container2}>
-        <Text>Welcome to the Light Cycle mobile app</Text>
-        <StatusBar style="auto" />
-      </View>
-      <TouchableOpacity onPress={() => testFunction}>
-        <Image style={styles.img} source={require("./left-filled.png")} />
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => testFunction}>
-        <Image style={styles.img} source={require("./right-filled.png")} />
-      </TouchableOpacity>
-    </>
-  );
+  return <ModalTemplate />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#f39c12",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  container2: {
-    flex: 1,
-    backgroundColor: "#34495e",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  img: {
-    width: 50,
-    height: 50,
-  },
-});
