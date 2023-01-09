959bc69f01781d3b26211f33d835a25a22480196
robin277t <robintyler87@gmail.com>
2023-01-09 14:57:20 +0000

    add boilerplate scratch testing code

--- mobile-dev/react-native-v1/App.js
+++ mobile-dev/react-native-v1/App.js
@@ -1,17 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
 
 export default function App() {
+  const testFunction = () => {
+    return <Text>LeftMate</Text>;
+  };
   return (
     <>
-    <View style={styles.container}>
-      <Text>Welcome to the Light Cycle mobile app</Text>
-      <StatusBar style="auto" />
-    </View>
-    <View style={styles.container2}>
-      <Text>Welcome to the Light Cycle mobile app</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <Text>Welcome to the Light Cycle mobile app</Text>
+        <StatusBar style="auto" />
+      </View>
+      <View style={styles.container2}>
+        <Text>Welcome to the Light Cycle mobile app</Text>
+        <StatusBar style="auto" />
+      </View>
+      <TouchableOpacity onPress={() => testFunction}>
+        <Image style={styles.img} source={require("./left-filled.png")} />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => testFunction}>
+        <Image style={styles.img} source={require("./right-filled.png")} />
+      </TouchableOpacity>
     </>
   );
 }
@@ -23,10 +32,14 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-    container2: {
+  container2: {
     flex: 1,
     backgroundColor: "#34495e",
     alignItems: "center",
     justifyContent: "center",
+  },
+  img: {
+    width: 50,
+    height: 50,
   },
 });
