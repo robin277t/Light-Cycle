5267e93b9d85030034c225efc8584c6dff4de6f9
Henry <boylanh3nry@gmail.com>
2023-01-11 14:36:23 +0000

    Buttons implemented on GridMakerGame

--- mobile-dev/react-native-v1/components/grid/GridMakerGame.js
+++ mobile-dev/react-native-v1/components/grid/GridMakerGame.js
@@ -6,6 +6,8 @@ import {
   View,
   TouchableOpacity,
   Image,
+  Modal,
+  Pressable,
 } from "react-native";
 import { Component, useEffect, useRef, useState } from "react";
 import { GameEngine } from "react-native-game-engine";
@@ -37,50 +39,197 @@ export default function GridMakerGame({
   let cycle2X = cycle2 % gridSide;
   let cycle2Y = (gridSide - cycle2X) / gridSide;
 
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const showModal = () => {
+    setModalVisible(true);
+    setTimeout(() => {
+      setModalVisible(false);
+    }, 1000);
+  };
+
   return (
-    <View style={styles.canvas}>
-      <GameEngine
-        ref={backingGrid}
-        style={{
-          width: gridSize,
-          height: gridSize,
-          flex: null,
-          backgroundColor: "grey",
-          position: "relative",
-          top: -50,
-        }}
-        key={"backingGrid"}
-      >
-        {wall.map((value) => {
-          if (value < 2) {
-            return (
-              <View
-                style={{
-                  width: cellSize,
-                  height: cellSize,
-                  backgroundColor: "pink",
-                  position: "absolute",
-                  left: gridPixelWidth - cellSize,
-                  top: 0,
-                }}
-                key={value}
-              ></View>
-            );
-          }
-        })}
-        <View
+    <>
+      <View style={styles.canvas}>
+        <GameEngine
+          ref={backingGrid}
           style={{
-            width: cellSize,
-            height: cellSize,
-            backgroundColor: "red",
+            width: gridSize,
+            height: gridSize,
+            flex: null,
+            backgroundColor: "grey",
             position: "relative",
-            left: 0,
-            top: 0,
+            top: -50,
           }}
-          key={"red"}
-        ></View>
-      </GameEngine>
-    </View>
+          key={"backingGrid"}
+        >
+          {wall.map((value) => {
+            if (value < 2) {
+              return (
+                <View
+                  style={{
+                    width: cellSize,
+                    height: cellSize,
+                    backgroundColor: "pink",
+                    position: "absolute",
+                    left: gridPixelWidth - cellSize,
+                    top: 0,
+                  }}
+                  key={value}
+                ></View>
+              );
+            }
+          })}
+          <View
+            style={{
+              width: cellSize,
+              height: cellSize,
+              backgroundColor: "red",
+              position: "relative",
+              left: 0,
+              top: 0,
+            }}
+            key={"red"}
+          ></View>
+        <View>
+          <Modal
+            animationType="none"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              Alert.alert("modal has been closed.");
+              setModalVisible(!modalVisible);
+            }}
+          >
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                <Text style={styles.modalText}>You clicked Top!</Text>
+                <Pressable
+                style={[styles.buttonOpen, styles.buttonClose]}
+                onPress={() => setModalVisible(!modalVisible)}
+                >
+                  <Text>Hide</Text>
+                </Pressable>
+              </View>
+            </View>
+          </Modal>
+        </View>
+        </GameEngine>
+        {/* <View>
+          <Modal
+            animationType="none"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              Alert.alert("modal has been closed.");
+              setModalVisible(!modalVisible);
+            }}
+          >
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                <Text style={styles.modalText}>You clicked Right!</Text>
+                <Pressable
+                  style={[styles.buttonOpen, styles.buttonClose]}
+                  onPress={() => setModalVisible(!modalVisible)}
+                >
+                  <Text>Hide</Text>
+                </Pressable>
+              </View>
+            </View>
+          </Modal>
+        </View>
+        <View>
+          <Modal
+            animationType="none"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              Alert.alert("modal has been closed.");
+              setModalVisible(!modalVisible);
+            }}
+          >
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                <Text style={styles.modalText}>You clicked Bottom!</Text>
+                <Pressable
+                  style={[styles.buttonOpen, styles.buttonClose]}
+                  onPress={() => setModalVisible(!modalVisible)}
+                >
+                  <Text>Hide</Text>
+                </Pressable>
+              </View>
+            </View>
+          </Modal>
+        </View>
+        <View>
+          <Modal
+            animationType="none"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              Alert.alert("modal has been closed.");
+              setModalVisible(!modalVisible);
+            }}
+          >
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                <Text style={styles.modalText}>You clicked Left!</Text>
+                <Pressable
+                  style={[styles.buttonOpen, styles.buttonClose]}
+                  onPress={() => setModalVisible(!modalVisible)}
+                >
+                  <Text>Hide</Text>
+                </Pressable>
+              </View>
+            </View>
+          </Modal>
+        </View> */}
+        <View style={styles.dpadtop}>
+          <TouchableOpacity
+            style={[styles.button, styles.buttonClose]}
+            onPress={() => showModal(!modalVisible)}
+          >
+            <Image
+              style={styles.img}
+              source={require("../../img/up-filled.png")}
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.dpadmiddle}>
+          <TouchableOpacity
+            style={[styles.button, styles.buttonClose]}
+            onPress={() => showModal(!modalVisible)}
+          >
+            <Image
+              style={styles.img}
+              source={require("../../img/left-filled.png")}
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.dpadmiddle}>
+          <TouchableOpacity
+            style={[styles.button, styles.buttonClose]}
+            onPress={() => showModal(!modalVisible)}
+          >
+            <Image
+              style={styles.img}
+              source={require("../../img/right-filled.png")}
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.dpadbottom}>
+          <TouchableOpacity
+            style={[styles.button, styles.buttonClose]}
+            onPress={() => showModal(!modalVisible)}
+          >
+            <Image
+              style={styles.img}
+              source={require("../../img/down_arrow.png")}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </>
   );
 }
 
@@ -120,5 +269,70 @@ const styles = StyleSheet.create({
     backgroundColor: "#c8ee2e",
     height: 5,
     width: 5,
+  },
+
+  button: {
+    borderRadius: 5,
+    padding: 8,
+    elevation: 1,
+  },
+
+  buttonOpen: {
+    backgroundColor: "#6273248",
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+
+  buttonClose: {
+    backgroundColor: "#D3D3D3",
+  },
+
+  img: {
+    width: 20,
+    height: 20,
+  },
+
+  dpadtop: {
+    flex: 0,
+    alignItems: "center",
+  },
+
+  dpadmiddle: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+  },
+
+  dpadbottom: {
+    flex: 1,
+    alignItems: "center",
+  },
+
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "#AF4f41",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
   },
 });
